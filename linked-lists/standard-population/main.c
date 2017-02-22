26acb4627eaa14002da13771e445da2756f7fd56
Brian Barto <bartobrian@gmail.com>
2017-02-22 13:32:59 -0500

    Standard list creation
    	new file:   linked-lists/standard-population/list.c
    	new file:   linked-lists/standard-population/list.h
    	new file:   linked-lists/standard-population/main.c

---
+++ linked-lists/standard-population/main.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "list.h"
+
+#define N 10
+
+int main(void) {
+	int i;
+	link head, x;
+	
+	head = new_link(0);
+	x = head;
+	
+	for (i = 1; i < N; ++i) {
+		x = insert_after(x, new_link(i));
+	}
+	
+	return 0;
+}

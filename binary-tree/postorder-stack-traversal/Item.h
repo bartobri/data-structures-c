75487abdfed52e39e42f0d5c536977c46d363138
Brian Barto <bartobrian@gmail.com>
2017-03-13 10:58:42 -0400

    Add postorder stack traversal for binary tree
    	new file:   binary-tree/postorder-stack-traversal/Item.h
    	new file:   binary-tree/postorder-stack-traversal/STACK.h
    	new file:   binary-tree/postorder-stack-traversal/TREE.h
    	new file:   binary-tree/postorder-stack-traversal/main.c
    	new file:   binary-tree/postorder-stack-traversal/stack.c
    	new file:   binary-tree/postorder-stack-traversal/tree.c

---
+++ binary-tree/postorder-stack-traversal/Item.h
@@ -0,0 +1,8 @@
+#ifndef ITEM_H
+#define ITEM_H 1
+
+#include "TREE.h"
+
+typedef tnode Item;
+
+#endif
---
+++ binary-tree/postorder-stack-traversal/STACK.h
@@ -0,0 +1,11 @@
+#ifndef STACK_H
+#define STACK_H 1
+
+#include "Item.h"
+
+void STACKinit(void);
+int  STACKcount(void);
+void STACKpush(Item);
+Item STACKpop(void);
+
+#endif

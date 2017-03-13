7d347a269df4efaf8cc5d998fd41aaf8cca3ac4a
Brian Barto <bartobrian@gmail.com>
2017-03-13 17:39:36 -0400

    Add algorithm for depth first recursive list traversal
    	new file:   graph/depth-first-recursive-list-traversal/GRAPH.h
    	new file:   graph/depth-first-recursive-list-traversal/graph.c
    	new file:   graph/depth-first-recursive-list-traversal/main.c

---
+++ graph/depth-first-recursive-list-traversal/graph.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "GRAPH.h"
+
+typedef struct node *link;
+struct node {
+	int v;
+	link next;
+};
+static link *adj;
+static int *visited;
+
+static link NEW(int v, link next) {
+	link x = malloc(sizeof(*x));
+	x->v = v;
+	x->next = next;
+	return x;
+}
+
+void graphInit(int S) {
+	int i;
+
+	adj = malloc(S * sizeof(link));
+	
+	for (i = 0; i < S; ++i) {
+		adj[i] = NULL;
+	}
+	
+	visited = malloc(S * sizeof(int));
+	
+	for (i = 0; i < S; ++i) {
+		visited[i] = 0;
+	}
+}
+
+void graphSet(int i, int j) {
+	adj[i] = NEW(j, adj[i]);
+	adj[j] = NEW(i, adj[j]);
+}
+
+void traverse(int k, void(*visit)(int)) {
+	link t;
+
+	(*visit)(k);
+	visited[k] = 1;
+	
+	for (t = adj[k]; t != NULL; t = t->next) {
+		if (visited[t->v] == 0) {
+			traverse(t->v, visit);
+		}
+	}
+}

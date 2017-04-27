e4134cf4de7af503eeed7f09b9ba6e03ed322af4
Brian Barto <bartobrian@gmail.com>
2017-04-26 23:15:51 -0400

    Uncomment select and sort function declarations.
    	modified:   symbol-table/linked-list-ordered/st.h

--- symbol-table/linked-list-ordered/st.h
+++ symbol-table/linked-list-ordered/st.h
@@ -8,7 +8,7 @@ int  STcount(void);
 void STinsert(Item);
 Item STsearch(Key);
 void STdelete(Item);
-//Item STselect(int);
-//void STsort(void (*visit)(Item));
+Item STselect(int);
+void STsort(void (*visit)(Item));
 
 #endif

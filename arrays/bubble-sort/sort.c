5fa73a436fbecf8aa3d3de04761c03b5adca2c6f
siddkataria <56654212+siddkataria@users.noreply.github.com>
2019-10-17 01:49:49 +0530

    added blank space

--- arrays/bubble-sort/sort.c
+++ arrays/bubble-sort/sort.c
@@ -4,10 +4,13 @@
 void bubble(Item a[], int l, int r) {
 	int i, j;
 	
-	for (i = l; i < r; ++i) {
-		for (j = r; j > i; --j) {
+	for (i = l; i < r; ++i)
+	{
+		for (j = r; j > i; --j) 
+		{
 			compexch(a[j-1], a[j]);
 		}
 	}
+	printf("    ");
 }
 

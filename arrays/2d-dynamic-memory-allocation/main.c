a9314eafa255df983471704bfd801da21e321e8a
siddkataria <56654212+siddkataria@users.noreply.github.com>
2019-10-17 01:44:43 +0530

    Update main.c

--- arrays/2d-dynamic-memory-allocation/main.c
+++ arrays/2d-dynamic-memory-allocation/main.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 int **malloc2d(int r, int c) {
-	int i;
+	int l;
 	int **t = malloc(r * sizeof(int *));
 	
-	for (i = 0; i < r; ++i) {
+	for (l = 0; l < r; ++l) {
 		t[i] = malloc(c * sizeof(int));
 	}
 	
@@ -13,16 +13,16 @@ int **malloc2d(int r, int c) {
 }
 
 int main(void) {
-	int i, j, k;
+	int l, j, k;
 	int x = 10;
 	int y = 10;
 	int z = 10;
 
 	// Testing 2d allocation
 	int **t = malloc2d(y, x);
-	for (i = 0; i < y; ++i) {
+	for (l = 0; l < y; ++l) {
 		for (j = 0; j < x; ++j) {
-			t[i][j] = 1;
+			t[l][j] = 1;
 		}
 	}
 	printf("2d Success\n");
